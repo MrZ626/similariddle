dfe76ed2b9aeca9a32715eb29a6eb0931666b0b2
MrZ_26 <1046101471@qq.com>
2022-04-17 15:58:25 +0800

    更新版本号

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return{
     ["appName"]="Similariddle",
-    ["apkCode"]=3,
-    ["verCode"]=010002,
+    ["apkCode"]=5,
+    ["verCode"]=010005,
 
-    ["appVer"]="V1.0.2",
-    ["verStr"]="1.0.2",
+    ["appVer"]="V1.0.5",
+    ["verStr"]="1.0.5",
 }
